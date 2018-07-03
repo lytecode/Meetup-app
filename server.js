6ff78a92a5523832c0db7441778b998574670c02
Basil <mbonubasil@gmail.com>
2018-07-03 22:51:43 +0100

    Add dotenv file

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express =  require('express');
+require('dotenv').config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
-	res.send('Homepage');
+	
+	res
+		.status(200)
+		.send('Homepage');
 });
 
+app.get('*', (req, res) => {
+	
+	res
+	   .status(404)
+	   .send('Page NotFound');
+});
 
 app.listen(PORT, () =>{
 	console.log(`app started on port ${PORT}`);
