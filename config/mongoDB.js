515732359f76167c78046002bf9354262dd54845
Basil <mbonubasil@gmail.com>
2018-08-14 23:11:55 +0100

    Add heroku port and mlab db

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 //connection to database
-mongoose.connect('mongodb://devsquare:dev12345@ds121182.mlab.com:21182/meetup');//('mongodb://localhost/meetup' || process.env.MongoDB_URI);
+mongoose.connect('mongodb://devsquare:dev12345@ds121182.mlab.com:21182/meetup' || 'mongodb://localhost/meetup');
 const db = mongoose.connection;
 db.on('connected', () => console.log('db connected successfully'));
 db.on('error', () => console.log('Ooops! Something went wrong with db connection'));
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const router = require('./app/routes/route'),
 const mongodb = require('./config/mongoDB');
 
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(urlencodedParser);
 app.use(express.static('public'));
